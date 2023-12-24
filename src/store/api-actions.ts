bbc7221f0fd2a2c48679f7722d08099597f2645a
iliana <ilianameit@gmail.com>
2023-12-24 19:33:42 +0300

    login, , reserve, booking slices

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -7,6 +7,10 @@ import { Quest } from '../types/quest';
 import { BookingInfo } from '../types/booking-info';
 import { BookingQuest } from '../types/booking-quest';
 import { redirectToRoute } from './actions';
+import { AuthData } from '../types/auth-data';
+import { UserData } from '../types/user-data';
+import { dropToken, saveToken } from '../services/token';
+import { Reservation } from '../types/reservation';
 
 type AsyncActionType = {
   dispatch: AppDispatch;
@@ -45,7 +49,7 @@ export const fetchBookingInfoQuestAction = createAsyncThunk<
   Quest['id'],
   AsyncActionType
 >(
-  'reservation/fetchInfoBookingQuest',
+  `${NameSpace.Booking}/fetchBookingInfoQuest`,
   async (id, { extra: api }) => {
     const { data } = await api.get<BookingInfo[]>(`${APIRoute.Quest}/${id}${APIRoute.Booking}`);
     return data;
@@ -53,17 +57,78 @@ export const fetchBookingInfoQuestAction = createAsyncThunk<
 );
 
 export const fetchBookQuestAction = createAsyncThunk<
-  BookingQuest,
+  Reservation,
   {
-    id: Quest['id'];
+    id: string;
     data: BookingQuest;
   },
   AsyncActionType
 >(
-  'reservation/fetchBookQuest',
+  `${APIRoute.Booking}/fetchBookQuest`,
   async (arg, { dispatch, extra: api }) => {
-    const { data } = await api.post<BookingQuest>(`${APIRoute.Quest}/${arg.id}${APIRoute.Booking}`, arg.data);
-    dispatch(redirectToRoute(AppRoutes.MyQuests));
+    const { data } = await api.post<Reservation>(`${APIRoute.Quest}/${arg.id}${APIRoute.Booking}`, arg.data);
+    dispatch(redirectToRoute(AppRoutes.Main));
     return data;
   },
+);
+
+export const fetchReservationQuestsAction = createAsyncThunk<
+  Reservation[],
+  undefined,
+  AsyncActionType
+>(
+  `${NameSpace.Reservation}/fetchReservationQuests`,
+  async (_arg, { extra: api }) => {
+    const { data } = await api.get<Reservation[]>(APIRoute.Reservation);
+    return data;
+  },
+);
+
+export const fetchCancelReservedQuest = createAsyncThunk<
+  Reservation,
+  Reservation['id'],
+  AsyncActionType
+>(
+  `${NameSpace.Reservation}/fetchCancelReservedQuest`,
+  async (id, { extra: api }) => {
+    const { data } = await api.delete<Reservation>(`${APIRoute.Reservation}/${id}`);
+    return data;
+  },
+);
+
+
+export const checkAuthAction = createAsyncThunk<
+  AuthData,
+  undefined,
+  AsyncActionType
+>(
+  `${NameSpace.User}/checkAuth`,
+  async (_arg, {extra: api}) => {
+    const { data } = await api.get<AuthData>(APIRoute.Login);
+
+    return data;
+  },
+);
+
+export const loginAction = createAsyncThunk<
+  AuthData,
+  UserData,
+  AsyncActionType
+  >(
+    `${NameSpace.User}/login`,
+    async ({email, password}, {extra: api}) => {
+      const {data} = await api.post<AuthData>(APIRoute.Login, {email, password});
+      const {token} = data;
+      saveToken(token);
+
+      return data;
+    }
+  );
+
+export const logoutAction = createAsyncThunk<void, undefined, AsyncActionType>(
+  `${NameSpace.User}/logout`,
+  async (_arg, { extra: api }) => {
+    await api.delete(APIRoute.Logout);
+    dropToken();
+  }
 );
